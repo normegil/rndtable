2c380425206f2a06500ec7f8e54d0b836c3592d2
Olivier Ory <mail@normegil.be>
2024-09-05 20:41:37 +0200

    Review Controller

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,12 @@
-use model::workspaces::{HierarchyElement, Workspace};
-use slint::{ModelRc, PlatformError, SharedString, VecModel};
+use ui::slint::SlintUI;
 
 slint::include_modules!();
 
 mod model;
+mod ui;
 
 fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
-
-    let active_model = model::Model::new();
-    let current_workspace = active_model
-        .workspaces
-        .get(0)
-        .ok_or(PlatformError::Other("Test".to_string()))?;
-    let current_workspace_name = current_workspace.name.as_str();
-    ui.set_workspaces(to_workspace_model(&active_model.workspaces));
-    ui.set_current_workspace(SharedString::from(current_workspace_name));
-
-    ui.set_generation_entries(to_entries_model(&current_workspace.hierarchy));
-
-    ui.on_clicked_generator_entry(|id| {
-        println!("Entry clicked: {id}");
-    });
-
+    let model = model::Model::new();
+    let ui = SlintUI::new(model)?;
     ui.run()
-}
-
-fn to_workspace_model(workspaces: &Vec<Workspace>) -> ModelRc<SharedString> {
-    let tmp = workspaces
-        .iter()
-        .map(|s| SharedString::from(&s.name))
-        .collect::<Vec<SharedString>>();
-    let tmp = VecModel::from(tmp);
-    ModelRc::new(tmp)
-}
-
-fn to_entries_model(entries: &Vec<HierarchyElement>) -> ModelRc<HierarchyEntry> {
-    let mut hierarchy_entry = vec![];
-    for element in entries {
-        hierarchy_entry.extend(flatten_entry(element, 0, "/"))
-    }
-
-    let tmp = VecModel::from(hierarchy_entry);
-    ModelRc::new(tmp)
-}
-
-fn flatten_entry(
-    entry: &HierarchyElement,
-    identation: i32,
-    parent_id: &str,
-) -> Vec<HierarchyEntry> {
-    match entry {
-        HierarchyElement::DashboardFolder(folder) => {
-            let current_id = parent_id.to_string() + "/" + &folder.name;
-            let mut elements = vec![HierarchyEntry {
-                id: SharedString::from(parent_id.to_string() + "/" + &folder.name),
-                title: SharedString::from(&folder.name),
-                folded: false,
-                identation,
-                is_folder: true,
-            }];
-
-            if !folder.folded {
-                for hierarchy_element in &folder.hierarchy {
-                    elements.extend(flatten_entry(
-                        hierarchy_element,
-                        identation + 1,
-                        &current_id,
-                    ))
-                }
-            }
-            return elements;
-        }
-        HierarchyElement::Dashboard(dashboard) => {
-            return vec![HierarchyEntry {
-                id: SharedString::from(parent_id.to_string() + "/" + &dashboard.name),
-                title: SharedString::from(&dashboard.name),
-                folded: false,
-                identation,
-                is_folder: false,
-            }];
-        }
-    }
 }
---
+++ src/ui/slint.rs
@@ -0,0 +1,114 @@
+use std::rc::Rc;
+
+use slint::{ModelRc, PlatformError, SharedString, VecModel};
+
+use crate::model::{
+    workspaces::{HierarchyElement, Workspace},
+    Model,
+};
+
+slint::include_modules!();
+
+pub struct SlintUI {
+    model: Model,
+    ui: AppWindow,
+}
+
+impl SlintUI {
+    pub fn new(model: Model) -> Result<Rc<SlintUI>, slint::PlatformError> {
+        let ui = AppWindow::new()?;
+        let slint_ui = Rc::new(SlintUI { model, ui });
+        slint_ui.init_data()?;
+        SlintUI::register_callbacks(slint_ui.clone());
+        Ok(slint_ui)
+    }
+
+    fn init_data(&self) -> Result<(), slint::PlatformError> {
+        let current_workspace = self
+            .model
+            .workspaces
+            .get(0)
+            .ok_or(PlatformError::Other("Test".to_string()))?;
+        let current_workspace_name = current_workspace.name.as_str();
+
+        self.ui
+            .set_workspaces(to_workspace_model(&self.model.workspaces));
+        self.ui
+            .set_current_workspace(SharedString::from(current_workspace_name));
+        self.ui
+            .set_generation_entries(to_entries_model(&current_workspace.hierarchy));
+        Ok(())
+    }
+
+    fn register_callbacks(slint_ui: Rc<SlintUI>) {
+        let clone_slint_ui = slint_ui.clone();
+        slint_ui.ui
+            .on_generator_entry_clicked(move |id| clone_slint_ui.generator_entry_clicked(id.to_string()));
+    }
+
+    fn generator_entry_clicked(&self, id: String) {
+        println!("Generator Entry clicked: {id}")
+    }
+
+    pub fn run(&self) -> Result<(), slint::PlatformError> {
+        self.ui.run()
+    }
+}
+
+fn to_workspace_model(workspaces: &Vec<Workspace>) -> ModelRc<SharedString> {
+    let tmp = workspaces
+        .iter()
+        .map(|s| SharedString::from(&s.name))
+        .collect::<Vec<SharedString>>();
+    let tmp = VecModel::from(tmp);
+    ModelRc::new(tmp)
+}
+
+fn to_entries_model(entries: &Vec<HierarchyElement>) -> ModelRc<HierarchyEntry> {
+    let mut hierarchy_entry = vec![];
+    for element in entries {
+        hierarchy_entry.extend(flatten_entry(element, 0, "/"))
+    }
+
+    let tmp = VecModel::from(hierarchy_entry);
+    ModelRc::new(tmp)
+}
+
+fn flatten_entry(
+    entry: &HierarchyElement,
+    identation: i32,
+    parent_id: &str,
+) -> Vec<HierarchyEntry> {
+    match entry {
+        HierarchyElement::DashboardFolder(folder) => {
+            let current_id = parent_id.to_string() + "/" + &folder.name;
+            let mut elements = vec![HierarchyEntry {
+                id: SharedString::from(parent_id.to_string() + "/" + &folder.name),
+                title: SharedString::from(&folder.name),
+                folded: false,
+                identation,
+                is_folder: true,
+            }];
+
+            if !folder.folded {
+                for hierarchy_element in &folder.hierarchy {
+                    elements.extend(flatten_entry(
+                        hierarchy_element,
+                        identation + 1,
+                        &current_id,
+                    ))
+                }
+            }
+            return elements;
+        }
+        HierarchyElement::Dashboard(dashboard) => {
+            return vec![HierarchyEntry {
+                id: SharedString::from(parent_id.to_string() + "/" + &dashboard.name),
+                title: SharedString::from(&dashboard.name),
+                folded: false,
+                identation,
+                is_folder: false,
+            }];
+        }
+    }
+}
